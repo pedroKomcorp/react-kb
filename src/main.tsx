83f6273c7719b5f2a1c9eea0f01fdee651885b84
pedroKomcorp <pedro.neto@komcorp.com.br>
2026-03-03 18:21:01 +0000

    Use ConfigProvider tokens for Popconfirm background

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd';
 import App from './app.tsx';
 import { AuthProvider } from './context/AuthContext.tsx'; // Import AuthProvider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ConfigProvider
+      theme={{
+        components: {
+          Popover: {
+            colorBgElevated: '#fff',
+            colorText: '#1f1e27',
+            colorTextHeading: '#1f1e27',
+          },
+          Popconfirm: {
+            colorText: '#1f1e27',
+            colorTextHeading: '#1f1e27',
+          },
+        },
+      }}
+    >
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ConfigProvider>
   </React.StrictMode>,
 );
